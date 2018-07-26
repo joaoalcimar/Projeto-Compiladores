f8902b4dc937b7d40fe15d53609c334ba87fcf73
bernardlcs <bernard.lenin4@gmail.com>
2018-07-26 02:48:50 -0300

    deleçoes de semantica

--- Compiladores/src/semantica/Parameter.java
+++
@@ -1,9 +0,0 @@
-package semantica;
-
-public class Parameter extends Variable {
-
-	public Parameter(String lexeme, Context context, String type, Object value) {
-		super(lexeme, context, type, value);
-		// TODO Auto-generated constructor stub
-	}
-}
